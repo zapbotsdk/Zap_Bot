3f5a8d2f4eaf816f0d8c2a3547c119c52e79e43d
zapbotsdk <kaveeshanee567@gmail.com>
2025-03-16 01:50:28 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,8 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "kaveeshanee567@gmail.com
+",
+  password: "Zap1515bot#",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
